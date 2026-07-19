77c59e5df896ff3e720992bbdcb3a4f545567453
Michał Rzodeczko <firelight.code@gmail.com>
2026-07-19 14:59:43 +0200

    feat: update HttpPaymentAdapterContractTest to include authentication properties and bump version to 1.0.0

--- src/test/java/com/rzodeczko/infrastructure/adapter/http/HttpPaymentAdapterContractTest.java
+++ src/test/java/com/rzodeczko/infrastructure/adapter/http/HttpPaymentAdapterContractTest.java
@@ -10,6 +10,7 @@ import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties
 import org.springframework.web.client.RestClient;
 
 import java.math.BigDecimal;
+import java.util.Map;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,7 +26,11 @@ class HttpPaymentAdapterContractTest {
             .downloadStub("com.app", "payment-service")
             .withPort(0)
             .stubsMode(StubRunnerProperties.StubsMode.REMOTE)
-            .repoRoot("https://maven.pkg.github.com/mrzodeczko-dev/payment-service");
+            .repoRoot("https://maven.pkg.github.com/mrzodeczko-dev/payment-service")
+            .withProperties(Map.of(
+                    "stubrunner.username", System.getenv().getOrDefault("GITHUB_ACTOR", ""),
+                    "stubrunner.password", System.getenv().getOrDefault("GITHUB_TOKEN", "")
+            ));
 
     private HttpPaymentAdapter adapter;
 
